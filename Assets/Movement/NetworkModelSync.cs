b333d283d860aa06fa66ca1589fd34986353c617
KahlenHernani <Kahlenhernani@gmail.com>
2026-07-01 13:24:25 -0400

    network

--- Assets/Movement/NetworkModelSync.cs
+++ Assets/Movement/NetworkModelSync.cs
@@ -22,7 +22,7 @@ public class NetworkModelSync : MonoBehaviour
 {
     public bool isHost = false;
 
-    public string serverIP = "ws://127.0.0.1:8080";
+    public string serverIP = "ws://255.255.255.240:8080";
 
     private WebSocket websocket;
 
